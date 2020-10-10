873e33ef0f72d032ff8ead626b2a015e498ce293
Samuel Bitancor Jr <BITANCOR.SAMUELJR@UE.EDU.PH>
2020-10-10 17:55:45 +0800

    adding first validation

--- script.js
+++ script.js
@@ -18,10 +18,28 @@ function register(){
     }
 }
 
+function validateAll(){
+    if (dataValidation){
+        document.form.submit();
+    }
+}
+
+function setErrorFor(input, message){
+    var formControl = input.parentElement;
+    var small = formControl.querySelector('small');
+    formControl.className = 'form-control error';
+    small.innerText = message;
+}
+
+function setSuccessFor(input){
+    var formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
 function dataValidation(){
     var lastNameValue = document.getElementById("lastName").value;
-    /*var firstNameValue = document.getElementById("firstName").value;
-    var midNameValue = document.getElementById("midname").value;
+    var firstNameValue = document.getElementById("firstName").value;
+    var midNameValue = document.getElementById("midName").value;
     var studNumValue = document.getElementById("studNum").value;
     var yearLevelValue = document.getElementById("yearLevel").value;
     var mobNumValue = document.getElementById("mobNum").value;
@@ -30,7 +48,76 @@ function dataValidation(){
     var usernameValue = document.getElementById("username").value;
     var passValue  = document.getElementById("pass").value;
     var rptPassValue  = document.getElementById("rptPass").value;
-    */
     
-    window.alert(lastNameValue);
+    var letters = /^[A-Za-z]+$/;
+    var numbers = /^[0-9]+$/;
+    
+    //Last Name
+    if (lastNameValue == ""){
+        setErrorFor(lastName, 'Last Name cannot be blank');
+        return false;
+    } else {
+        setSuccessFor(lastName);
+    }
+    if (letters.test(lastNameValue)){
+        setSuccessFor(lastName);
+    } else {
+        setErrorFor(lastName, 'Alphabets Only');
+        return false;
+    }
+    
+    //First Name
+    if (firstNameValue == ""){
+        setErrorFor(firstName, 'First Name cannot be blank');
+        return false;
+    } else {
+        setSuccessFor(firstName);
+    }
+    if (letters.test(firstNameValue)){
+        setSuccessFor(firstName);
+    } else {
+        setErrorFor(firstName, 'Alphabets Only');
+        return false;
+    }
+    
+    //Middle Initial
+    if (letters.test(midNameValue) || midNameValue==""){
+        setSuccessFor(midName);
+    } else {
+        setErrorFor(midName, 'Alphabets Only');
+        return false;
+    }
+    
+    //Student Number
+    if (numbers.test(studNumValue)){
+        setSuccessFor(studNum);
+    } else {
+        setErrorFor(studNum, 'Numeric Characters Only');
+        return false;
+    }
+    
+    
+    var testing = Number(document.getElementById("bday"));
+    //Birth Date
+    if (bdayValue >= 2002-10-10){
+        setErrorFor(bday, 'Legal Age Only');
+        return false;
+        
+    } else {
+        setSuccessFor(bday);
+    }
+    
+    
+    alert(testing);
+    
+    //Mobile Number
+    if (numbers.test(mobNumValue)){
+        setSuccessFor(mobNum);
+    } else {
+        setErrorFor(mobNum, 'Numeric Characters Only');
+        return false;
+    }
+    
+    
+    
 }
